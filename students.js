5ebd849a00f8859ba9f99b39780b989e59440200
GiorgosGi <73617921+GiorgosGi@users.noreply.github.com>
2021-09-04 12:47:41 +0300

    Update students.js

--- students.js
+++ students.js
@@ -1,18 +1,17 @@
 var student_date_of_birth = document.getElementById('student_date_of_birth');
 
-
-    var todayMinus18years = new Date();
-    var dd = todayMinus18years.getDate();
-    var mm = todayMinus18years.getMonth()+1; 
-    var yyyy = todayMinus18years.getFullYear();
-     if(dd<10){
-            dd='0'+dd
+var todayMinus18years = new Date();
+var dd = todayMinus18years.getDate();
+var mm = todayMinus18years.getMonth()+1; 
+var yyyy = todayMinus18years.getFullYear();
+if(dd<10){
+          dd='0'+dd
         } 
-        if(mm<10){
-            mm='0'+mm
+if(mm<10){
+          mm='0'+mm
         } 
     
-    todayMinus18years = yyyy-18 +'-'+mm+'-'+dd;    
+todayMinus18years = yyyy-18 +'-'+mm+'-'+dd;    
 
 student_date_of_birth.setAttribute("value", todayMinus18years);
 
